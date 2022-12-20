41143943ece68e23e894e9b331831db83928a235
KhantKyiThar <khantkyithar2.6.97@gmail.com>
2022-12-21 01:31:18 +0630

    Staff API

--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/StaffService.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/StaffService.java
@@ -1,5 +1,19 @@
 package com.javaclass.restaurant.service;
 
+import java.util.List;
+
+import com.javaclass.restaurant.entity.Staff;
+
 public interface StaffService {
 
+	public List<Staff> getAll();
+
+	public Staff get(int id);
+
+	public Staff create(Staff food);
+
+	public Staff update(int id, Staff food);
+
+	public boolean delete(int id);
+	
 }
--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/StaffServiceImpl.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/StaffServiceImpl.java
@@ -1,5 +1,60 @@
 package com.javaclass.restaurant.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaclass.restaurant.entity.Staff;
+import com.javaclass.restaurant.repository.StaffRepo;
+
+@Service
 public class StaffServiceImpl implements StaffService {
+
+	@Autowired
+	StaffRepo staffRepo;
+
+	@Override
+	public List<Staff> getAll() {
+		return staffRepo.findAll();
+	}
+
+	@Override
+	public Staff get(int id) {
+		return staffRepo.findById(id).orElse(null);
+	}
+
+	@Override
+	public Staff create(Staff staff) {
+		staff.setCreatedAt(LocalDateTime.now());
+		return staffRepo.save(staff);
+	}
+
+	@Override
+	public Staff update(int id, Staff staff) {
+		Staff toUpdateStaff = get(id);
+		if (toUpdateStaff != null) {
+			toUpdateStaff.setName(staff.getName());
+			toUpdateStaff.setLoginId(staff.getLoginId());
+			toUpdateStaff.setPassword(staff.getPassword());
+			toUpdateStaff.setPhone(staff.getPhone());
+			toUpdateStaff.setGender(staff.getGender());
+			toUpdateStaff.setStaffType(staff.getStaffType());
+			toUpdateStaff.setUpdatedAt(LocalDateTime.now());
+			return staffRepo.save(toUpdateStaff);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		Staff staff = get(id);
+		if (staff != null) {
+			staffRepo.deleteById(id);
+			return true;
+		}
+		return false;
+	}
 
 }
