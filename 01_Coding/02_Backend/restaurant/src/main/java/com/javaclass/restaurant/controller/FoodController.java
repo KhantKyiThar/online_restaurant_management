f74cd8fe1d91b2ececacf8288c5a551dad219b75
khine-phyu <110364741+khine-phyu@users.noreply.github.com>
2022-12-22 22:21:10 +0630

    category api

--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/controller/FoodController.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/controller/FoodController.java
@@ -1,5 +1,22 @@
 package com.javaclass.restaurant.controller;
 
-public class FoodController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaclass.restaurant.entity.Food;
+import com.javaclass.restaurant.service.FoodService;
+
+@RestController
+public class FoodController {
+	@Autowired
+	private FoodService foodService;
+	
+	//Get
+		@GetMapping(path = "/api/food")
+		public List<Food> getAll() {
+			return foodService.getAll();
+		}
 }
--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/CategoryServiceImpl.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/service/CategoryServiceImpl.java
@@ -1,5 +1,45 @@
 package com.javaclass.restaurant.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javaclass.restaurant.entity.Category;
+import com.javaclass.restaurant.repository.CategoryRepo;
+
+
+@Service
 public class CategoryServiceImpl implements CategoryService {
+	
+	@Autowired
+	CategoryRepo categroyRepo;
+
+	@Override
+	public List<Category> getAll() {
+		return categroyRepo.findAll();
+	}
+
+	@Override
+	public Category get(int id) {
+		return categroyRepo.findById(id).orElse(null);
+	}
+
+	@Override
+	public Category create(Category category) {
+		return categroyRepo.save(category);
+	}
+
+	@Override
+	public Category update(int id, Category category) {
+		category.setId(id);
+		return categroyRepo.save(category);
+	}
+
+	@Override
+	public boolean delete(int id) {
+		categroyRepo.deleteById(id);
+		return true;
+	}
 
 }
