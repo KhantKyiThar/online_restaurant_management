2c141012e9a7fa16e432dc4239e0409abfc65843
AMMW <ayemohmohwin92@gmail.com>
2022-12-25 23:09:37 +0630

    ammw - add file api in adminController

--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/controller/AdminController.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/controller/AdminController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaclass.restaurant.entity.Food;
 import com.javaclass.restaurant.entity.Staff;
@@ -47,6 +49,24 @@ public class AdminController {
 	@PostMapping("/food/create")
 	public Food foodCreate(@Valid @RequestBody Food food) {
 		return foodService.create(food);
+	}
+	
+	@PostMapping("/file/create")
+	public ResponseEntity<String> createFile(@RequestParam("file") MultipartFile file,@RequestParam("fileType") String fileType){
+		String filePath=storageService.create(file, fileType);
+		if(filePath==null) {
+			return ResponseEntity.internalServerError().build();
+		}
+		return ResponseEntity.ok(filePath);
+	}
+	
+	@PutMapping("/file/update")
+	public ResponseEntity<String> updateFile(@RequestParam("file") MultipartFile file,@RequestParam("fileType") String fileType,@RequestParam("filePath")String filePath){
+		String newFilePath=storageService.update(file, fileType, filePath);
+		if(newFilePath==null) {
+			return ResponseEntity.internalServerError().build();
+		}
+		return ResponseEntity.ok(newFilePath);
 	}
 
 	@PutMapping("/food/update/{id}")
