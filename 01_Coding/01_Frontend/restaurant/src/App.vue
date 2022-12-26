6e27f461e18728d0ec6a191555e5394c235221b5
AMMW <ayemohmohwin92@gmail.com>
2022-12-26 16:55:37 +0630

    ammw - frontend

---
+++ 01_Coding/01_Frontend/restaurant/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar color="deep-purple accent-4" dark absolute>
+      <v-toolbar-title>Restaurant Management</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+      Logout
+    </v-btn>
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <v-footer color="deep-purple accent-4" dark padless>
+      <v-col class="text-center" cols="12">
+        <strong>Team GigaBytes</strong>
+      </v-col>
+  </v-footer> 
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
