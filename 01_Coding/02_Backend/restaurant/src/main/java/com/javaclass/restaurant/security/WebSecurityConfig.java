362d083e213967fdab7a1bb1717071659db02eda
AMMW <ayemohmohwin92@gmail.com>
2022-12-26 21:57:12 +0630

    ammw - webConfig

--- 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/security/WebSecurityConfig.java
+++ 01_Coding/02_Backend/restaurant/src/main/java/com/javaclass/restaurant/security/WebSecurityConfig.java
@@ -2,14 +2,29 @@ package com.javaclass.restaurant.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().mvcMatchers("/**").permitAll();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.debug(false).ignoring()
+				.antMatchers("/images/**", "/js/**", "/css/**");
+	}
+	
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
